842df9091c42125e3e1887b9b2ace0795e1f270a
larrykwon <koallarry11@kaist.ac.kr>
2024-09-29 02:43:16 +0900

    add more tests

---
+++ assign1/dist/tests/test-l/cwd-single-long.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from tests.test import Test, weight
+import random
+import string
+
+TEST_DIR_NAMES = ['.']
+TEST_PLAIN_FILE_NAMES = ["plain-file-1", "plain-file-2", "plain-file-3"]
+TEST_BIN_FILE_NAMES = ["bin-file-1", "bin-file-2"]
+
+@weight(15)
+class Test(Test):
+    def __init__(self, root_dir, bin_path, ref_bin_path):
+        super().__init__(root_dir, bin_path, ref_bin_path)
+
+        random.seed(13100)
+        for name in TEST_PLAIN_FILE_NAMES:
+            size = random.randint(0x100, 0x1000)
+            plain_payload = self.random_string(size)
+            self.create_file(name, plain_payload.encode())
+        for name in TEST_BIN_FILE_NAMES:
+            size = random.randint(0x100, 0x1000)
+            self.create_file(name, self.random_bytes(size))
+        return
+
+    def run(self):
+        result = self.run_procs_and_diff(["-l"], self.test_dir, is_long = True)
+        return not result, result
